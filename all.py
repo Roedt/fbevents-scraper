0f7e81984c121ea8667229e83a3f7d1931467e5b
Mads Opheim <mads.opheim@gmail.com>
2019-04-13 18:49:00 +0200

    Finally found the address in the facebook response. Replacing the postnumber search with parsing of this

--- all.py
+++ all.py
@@ -82,17 +82,21 @@ class FacebookEventSpider(scrapy.Spider):
         trimmed = trimmed.replace('<del><del>', '')
         return trimmed
 
-    def parseAddressToPostnumber(self, address):
-        parsed = re.search('\d{4}', address)
-        if (parsed):
-            return parsed.group(0)
-        return ''
-
     def parseSingleEvent(self, response):
         try:
             self.parseSingleEventInner(response)
         except Exception as e:
             print(e)
+
+    def getPositionFromMap(self, html):
+        try:
+            lmindex = html.index("26daddr%3D")
+            pos = html[lmindex+10:lmindex+45].split('%252C')
+            lat = pos[0]
+            lon = pos[1].split('%')[0]
+            return {"lat": lat, "lon": lon}
+        except ValueError as e:
+            return None
 
     def parseSingleEventInner(self, response):
         html_str = response.body.decode('unicode-escape')
@@ -118,14 +122,11 @@ class FacebookEventSpider(scrapy.Spider):
             parsedEvent['address'] = fullLocation[1]
         else:
             parsedEvent['address'] = ''
-        parsedEvent['postnumber'] = self.parseAddressToPostnumber(parsedEvent['address'])
-        if len(parsedEvent['postnumber']) == 0:
-            parsedEvent['postnumber'] = self.parseAddressToPostnumber(parsedEvent['location'])
-        if runningLocally == False:
-            if len(parsedEvent['postnumber']) > 0:
-                position = getPosition(parsedEvent['postnumber'])
-                parsedEvent['lat'] = position['lat']
-                parsedEvent['lon'] = position['lon']
+
+        positionFromMap = self.getPositionFromMap(html_str)
+        if positionFromMap is not None:
+            parsedEvent['lat'] = positionFromMap['lat']
+            parsedEvent['lon'] = positionFromMap['lon']
 
         parsedEvent['host'] = self.target_username
         
@@ -214,71 +215,6 @@ def fetch():
     d = runner.join()
     d.addBoth(lambda _: reactor.stop())
     reactor.run()
-
-
-CONNECTION_NAME = getenv(
-  'INSTANCE_CONNECTION_NAME',
-  'facebookevents:europe-west1:postnumber')
-DB_USER = getenv('MYSQL_USER', 'read')
-DB_PASSWORD = getenv('MYSQL_PASSWORD', 'AllowedToRead')
-DB_NAME = getenv('MYSQL_DATABASE', 'postnummer')
-
-
-def _getCursorClass():
-    if runningLocally:
-        return ''
-    return pymysql.cursors.DictCursor
-
-mysql_config = {
-  'user': DB_USER,
-  'password': DB_PASSWORD,
-  'db': DB_NAME,
-  'charset': 'utf8mb4',
-  'cursorclass': _getCursorClass(),
-  'autocommit': True
-}
-
-# Create SQL connection globally to enable reuse
-# PyMySQL does not include support for connection pooling
-mysql_conn = None
-
-
-def __get_cursor():
-    """
-    Helper function to get a cursor
-      PyMySQL does NOT automatically reconnect,
-      so we must reconnect explicitly using ping()
-    """
-    try:
-        return mysql_conn.cursor()
-    except OperationalError:
-        mysql_conn.ping(reconnect=True)
-        return mysql_conn.cursor()
-
-def getPosition(postnumber):
-    global mysql_conn
-
-    # Initialize connections lazily, in case SQL access isn't needed for this
-    # GCF instance. Doing so minimizes the number of active SQL connections,
-    # which helps keep your GCF instances under SQL connection limits.
-    if not mysql_conn:
-        try:
-            mysql_conn = pymysql.connect(**mysql_config)
-        except OperationalError:
-            # If production settings fail, use local development ones
-            mysql_config['unix_socket'] = f'/cloudsql/{CONNECTION_NAME}'
-            mysql_conn = pymysql.connect(**mysql_config)
-
-    # Remember to close SQL resources declared while running this function.
-    # Keep any declared in global scope (e.g. mysql_conn) for later reuse.
-    with __get_cursor() as cursor:
-        cursor.execute('SELECT lat, lon from postnummer where postnr = ' + postnumber)
-        results = cursor.fetchone()
-        toReturn = {}
-        toReturn['lat'] = str(results['lat'])
-        toReturn['lon'] = str(results['lon'])
-        return toReturn
-
 
 def run(d, f):
     fetch()
